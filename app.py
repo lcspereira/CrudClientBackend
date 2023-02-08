cd2139f7e6985805e13c43432396f0e6a28c4b11
Lucas Pereira <lucas.pereira6c@gmail.com>
2023-02-08 00:31:59 -0300

    [ENH] Implementação todos os serviços backend em docker

--- app.py
+++ app.py
@@ -4,11 +4,10 @@ from models import db
 from api.cliente import ClienteListResource, ClienteResource
 from api.item import ItemListResource, ItemResource, ItemListByClienteResource
 from api import api
+import os
 
 app = Flask(__name__)
-app.config[
-    "SQLALCHEMY_DATABASE_URI"
-] = "postgresql+psycopg2://postgres:crudclient@127.0.0.1:5432/crud"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("CRUD_DB_URL")
 
 db.init_app(app)
 api.init_app(app)
@@ -17,4 +16,4 @@ api.init_app(app)
 migrate = Migrate(app, db)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000, host="0.0.0.0")
